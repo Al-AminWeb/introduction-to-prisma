5a630c7c9b7dec474968c33c2adebd2c3864a6b5
Al-AminWeb <mdalaminweb.1@gmail.com>
2025-12-24 22:43:37 +0600

    creating data using prisma

---
+++ crud.ts
@@ -0,0 +1,32 @@
+import {prisma} from "./lib/prisma";
+
+
+async function run(){
+    // const createUser = await prisma.user.create({
+    //     data: {
+    //         name:"alif islam",
+    //         email:"alif@nxt.com"
+    //     }
+    // })
+    // console.log("User created:", createUser);
+
+    // const createPost = await prisma.post.create({
+    //     data:{
+    //         title:"My first post",
+    //         content:"This is the content of my first post",
+    //         isPublished:true,
+    //         authorId:1
+    //     }
+    // })
+    // console.log("Post created:", createPost);
+
+    const createProfile = await prisma.profile.create({
+        data:{
+            bio:"This is my bio",
+            userId:1
+        }
+    })
+    console.log("Profile created:", createProfile);
+}
+
+run()
